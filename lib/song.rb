ef31ef1a8196887d87b771f94587fa7b406f91f8
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-07 16:38:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     artist = Artist.find_or_create_by_name(parse[0])
     song = Song.new(parse[1])
     artist.add_song(song)
-    
+    song.artist = artist 
   end 
   
   def artist_name=(name)
