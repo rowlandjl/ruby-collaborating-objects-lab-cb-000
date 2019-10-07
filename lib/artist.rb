abf26f732acba2342b4ce24f0121765f67fc2bc9
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-07 16:04:08 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(name)
+    @name = name 
+    self.class.all << self 
+  end 
+  
+  def add_song(song)
+    song.artist = self 
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self} 
+  end 
+  
+  def self.find_or_create_by_name(name)
+  end 
+  
+  def print_songs 
+    self.songs.collect {|song| song.name }
+  end 
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(name)
+    @name = name 
+    self.class.all << self 
+  end 
+  
+  def self.new_by_filename 
+  end 
+  
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist == Artist.new(name)
+    else 
+      self.artist.name = name 
+    end 
+  end 
+  
+end 
