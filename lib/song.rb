ca4c5bec300cf6761a415767e633e4921e62a2f7
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-07 16:43:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,11 +22,8 @@ class Song
   end 
   
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else 
-      self.artist.name = name 
-    end 
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist 
   end 
   
 end 
